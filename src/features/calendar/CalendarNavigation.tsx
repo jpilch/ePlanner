cda2f8f51474c723dd3ddd3bb842b06fac79c41f
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-12-08 10:59:29 +0100

    add calendar navigation bar

--- src/features/calendar/CalendarNavigation.tsx
+++ src/features/calendar/CalendarNavigation.tsx
@@ -1,7 +1,25 @@
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+
 import styles from "./Calendar.module.scss";
+
 
 export default function CalendarNavigation() {
     return (
-        <div className={styles.navigation}></div>
+        <div className={styles.navigation}>
+            <CalendarNavigation.Icon>
+                <ArrowLeftIcon />
+            </CalendarNavigation.Icon>
+            <p className={styles.navigation__month}>December 2022</p>
+            <CalendarNavigation.Icon>
+                <ArrowRightIcon />
+            </CalendarNavigation.Icon>
+        </div>
+    )
+}
+
+CalendarNavigation.Icon = function ({ children }: { children: JSX.Element }) {
+    return (
+        <div className={styles.navigation__icon}>{children}</div>
     )
 }
